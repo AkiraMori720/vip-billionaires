46dcabaf563280177b1ba3f48dfc887aac847905
Hosokawa Zen <56865797+hosokawa-zen@users.noreply.github.com>
2021-11-10 09:40:40 +0800

    version 1.0

---
+++ Admin/functions/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stripe_private_key: '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',
+    mailer_host: 'smtp.gmail.com',
+    mailer_port: 465,
+    mailer_user: 'kzar1102@gmail.com',
+    mailer_client_id: '729621830311-0o6ti4k77ud12dqrsg2l9vi4ajvldh64.apps.googleusercontent.com',
+    mailer_client_secret: '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',
+    mailer_refresh_token: '1//046DJ8KRHJQUOCgYIARAAGAQSNwF-L9Ir5yfJPxim1LPgyCuWEjA7Y6o80bkuefK79h3BEMWqmdu5_vu8k1_iXr6rgsgaxZu_Omc'
+}
